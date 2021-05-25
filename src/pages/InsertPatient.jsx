9c923e0fa8afff3c17bd7e1a98a82f30d4f5a4a6
Pedro Augusto de Sousa Negrão <pedron_1997@hotmail.com>
2021-05-25 15:12:08 -0300

    Update InsertPatient.jsx

--- src/pages/InsertPatient.jsx
+++ src/pages/InsertPatient.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
-import { Typography, Button, Grid, Paper, Avatar} from "@material-ui/core";
+import { 
+    Typography,
+    Button,
+    Grid,
+    Paper,
+    Avatar,
+    TextField,
+    FormControl,
+    FormControlLabel,
+    RadioGroup,
+    Radio
+} from "@material-ui/core";
 import useStyles from '../styles';
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 
@@ -19,8 +30,31 @@ const InsertPatient = () => {
                         <Typography variant="h6" align="center" color="textPrimary" gutterBottom>
                             Please fill this form to add a Patient to the list
                         </Typography>
-                        
                     </Grid>
+                    <form>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Patient Name"
+                            name="name"
+                            autoComplete="name"
+                            autoFocus
+                        />
+                    </form>
             </Paper>
         </Grid>
     );
